f84ffbadc8c16a6f3be900f21c51830c30475949
Christian Payne <christianpayne522@gmail.com>
2025-05-18 17:49:44 -0700

    Working start and stop times.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![allow(unused_assignments)]
 
 use anyhow::Result;
-use chrono::{Date, DateTime, Duration, NaiveDate, Utc};
-use clap::{Parser, Subcommand};
+use chrono::Local;
+use clap::Parser;
 use obws::Client;
 
 #[derive(Parser)]
 struct Args {
     #[arg(short, long)]
-    start_time: u64,
+    start_time: Option<u64>,
     #[arg(short, long)]
     end_time: u64,
 }
@@ -33,18 +33,21 @@ async fn main() -> Result<()> {
         recording_state = RecordingState::Stopped;
     }
 
-    dbg!(Utc::now().timestamp_millis());
+    let start_time = args
+        .start_time
+        .unwrap_or(Local::now().timestamp_millis() as u64);
+    let end_time = args.end_time;
 
     loop {
         match recording_state {
             RecordingState::Stopped => {
-                if Utc::now().timestamp_millis() > args.start_time as i64 {
+                if Local::now().timestamp_millis() > start_time as i64 {
                     client.recording().start().await?;
                     recording_state = RecordingState::Started;
                 }
             }
             RecordingState::Started => {
-                if Utc::now().timestamp_millis() > args.end_time as i64 {
+                if Local::now().timestamp_millis() > end_time as i64 {
                     client.recording().stop().await?;
                     recording_state = RecordingState::Stopped;
                     break;
